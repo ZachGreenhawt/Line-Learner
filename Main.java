00575e33208b0ce6bd6a1f6456cb05f1e7832533
ZachGreenhawt <Zachgreenhawt@gmail.com>
2025-12-18 17:11:05 -0700

    Changed method name to lineProcessor

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         ScriptStore.scriptToRead(args);
-        ScriptStore.lineCounter(args);
+        ScriptStore.lineProcessor(args);
     }
 }
