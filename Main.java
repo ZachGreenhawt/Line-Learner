cc64ed28c026b75b1ca6f39fbd43fe29e54a87e0
ZachGreenhawt <Zachgreenhawt@gmail.com>
2025-12-18 18:00:24 -0700

    Changed to properly print out the data

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         ScriptStore.scriptToRead(args);
-        ScriptStore.lineProcessor(args);
+        ParsedScript parsed = ScriptStore.lineProcessor(args);
+        System.out.println(parsed.getCharName()+ " has " + parsed.size() + " lines.");
+        for (int i = 0; i < parsed.size(); i++){
+            System.out.println( "Cue line: ");
+            System.out.println(parsed.getCue(i));
+            System.out.println(parsed.getCharName() + "'s line " + (i+1) + ":");
+            System.out.println(parsed.getCharLine(i));
+        }
     }
 }
