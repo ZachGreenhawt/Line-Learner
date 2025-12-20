bd9c8a0263b350de8c911ce5d6483518020dec84
ZachGreenhawt <Zachgreenhawt@gmail.com>
2025-12-19 19:31:55 -0700

    updated to match changes

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        ScriptStore.scriptToRead(args);
-        ParsedScript parsed = ScriptStore.lineProcessor(args);
-        PracticeSession.run(parsed);
+        Settings settings = Settings.menu();
+        ScriptToString.scriptToRead(args);
+        ParsedScript parsed = ScriptParser.lineProcessor(args, settings);
+        PracticeSession.run(parsed, settings);
     }
 }
