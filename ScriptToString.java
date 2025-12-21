f92c6ecebfe3a1a6117f7b1b68d1219aaf0e14d9
ZachGreenhawt <Zachgreenhawt@gmail.com>
2025-12-20 17:51:03 -0700

    Added pdf compatibility

--- ScriptToString.java
+++ ScriptToString.java
@@ -1,31 +1,62 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class ScriptToString {
     public static String scriptToRead(Scanner sc) {
-        StringBuilder processedScript = new StringBuilder();//init processedScript; string builder
-        System.out.println("Please enter to number that corresponds with your file extension:\n[1] .txt\n*note that the default is .txt; if there is an error, program will default to .txt*");
-        String fileExtension = sc.nextLine();
-        if(fileExtension.equals("1")){
-            fileExtension = ".txt";
-        } else{
-            fileExtension = ".txt";
+        System.out.println("Please enter the number that corresponds with your file type:\n[1] .txt\n[2] .pdf\n(Default is 1)");
+        String choice = sc.nextLine().trim();
+        String fileExtension = choice.equals("2") ? ".pdf" : ".txt";
+        String fileName = "";
+        while (fileName.isEmpty()) {
+            System.out.println("Enter the file name of your script.\nPlease exclude the file extension:");
+            fileName = sc.nextLine().trim();
         }
-        System.out.println("Enter the file name of your script.\nPlease exclude the file extension: ");
-        String fileName = sc.nextLine().trim(); //take file name
-        File script = new File("Example-Scripts/"+ fileName + fileExtension); //this is the script utilizing the name
+        File script = new File("Example-Scripts/" + fileName + fileExtension);
+        if (!script.exists()) {
+            System.out.println("Could not find file: " + script.getPath());
+            return "";
+        }
+        if (fileExtension.equals(".txt")) {
+            return readTxt(script);
+        }
+        return readPdf(script);
+    }
+
+    private static String readTxt(File script) {
+        StringBuilder processedScript = new StringBuilder();
         try (Scanner fileScanner = new Scanner(script)) {
             while (fileScanner.hasNextLine()) {
-                processedScript.append(fileScanner.nextLine());//take the scan of the script, append it
-                if (fileScanner.hasNextLine()) {//if there is a next line, append \n for a next line
+                processedScript.append(fileScanner.nextLine());
+                if (fileScanner.hasNextLine()) {
                     processedScript.append("\n");
                 }
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Could not find file: " + script.getName());
+        } catch (Exception e) {
+            System.out.println("Could not read txt file: " + script.getName());
             return "";
         }
         return processedScript.toString();
+    }
+
+    private static String readPdf(File script) {
+        try (PDDocument pdf = Loader.loadPDF(script)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            stripper.setSortByPosition(true);
+            String text = stripper.getText(pdf);
+            // DEBUG: dump raw PDF text
+            System.out.println("---- PDF TEXT DEBUG START ----");
+            System.out.println(text.replace("\r\n", "\n").replace("\r", "\n"));
+            System.out.println("---- PDF TEXT DEBUG END ----");
+            return text.replace("\r\n", "\n").replace("\r", "\n");
+        } catch (IOException e) {
+            System.out.println("Could not read PDF: " + script.getName());
+            System.out.println("Error: " + e.getMessage());
+            return "";
+            
+        }
     }
 }
